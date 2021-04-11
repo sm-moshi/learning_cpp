aa9b542d5238ee9bdd3f02b70cd89961041cf658
sm_moshi <meya@posteo.se>
2021-04-11 13:24:10 +0200

    Create Palindrome_Numbers.cpp

---
+++ Palindrome_Numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+    int a, z = 0;
+    a = x;
+    
+    while (a != 0)
+		{
+			z *= 10;
+			z = z + a % 10;
+			a = a / 10;
+		}
+	if (x == z)
+		return isPalindrome;
+	else
+		return false;
+}
+
+int main() {
+    int n;
+    cin >>n;
+    
+    if(isPalindrome(n)) {
+        cout << n <<" is a palindrome";
+    }
+    else {
+        cout << n <<" is NOT a palindrome";
+    }
+    return 0;
+}
