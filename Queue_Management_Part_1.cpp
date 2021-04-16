4fbb121ef98b7af8c8fcd3690a8fbb82684e0bcc
sm_moshi <meya@posteo.se>
2021-04-16 20:23:20 +0200

    Update Queue_Management_Part_1.cpp

--- Queue_Management_Part_1.cpp
+++ Queue_Management_Part_1.cpp
@@ -21,7 +21,7 @@ class Queue {
 	}
 	void remove() { 
 		if (size == 0) { 
-			cout << "Queue is empty"<<endl; 
+			cout << "The queue is empty!" << endl; 
 			return; 
 		} 
 		else { 
@@ -33,13 +33,13 @@ class Queue {
 	} 
 	void print() { 
 		if (size == 0) { 
-			cout << "Queue is empty"<<endl; 
+			cout << "The queue is empty!" << endl; 
 			return; 
 		} 
 		for (int i = 0; i < size; i++) { 
-			cout<<queue[i]<<" <- ";
+			cout << queue[i] << " <- ";
 		} 
-		cout <<endl;
+		cout << endl;
 	}
 	void add(int x) {
 		size += 1;
